b042988688fe3de6f7790d4cb6c124e89d3ce232
GMTurbo <gmturbo@me.com>
2014-04-10 22:00:08 -0700

    Player Matching Added
    Added matching system for players to matchup based off a matchCode.
    Tested as well.

--- playerTable.js
+++ playerTable.js
@@ -1,10 +1,59 @@
 
+var Table = require("cli-table");
 function playerTable(){
     this._list = [];
     /* entries should look like this
     *   _list[JSON.stringify(socket)] = {socket: socket, playerId:int, matched: bool};
     *
     */
+
+    //to the given socket, attach the match token
+    this.addMatchToken = function(socket, token){
+      var info = this.getSocketInfo(socket);
+      if( info && token > 0 && !info.matched){
+        info.matchCode = token;
+        this.checkForMatch(socket);
+        return true;
+      }else if (token <= 0){
+        console.log("token must be positive value > 0")
+      }else if(info.matched){
+        console.log("socket is already matched up.  Disconnect first");
+      }
+      return false;
+    };
+
+    //check for an available match, requires the socket,
+    //if one is found, it returns the socket to you
+    this.checkForMatch = function(socket){
+      var info = this.getSocketInfo(socket);
+      if(info.matchCode){
+        var list = this._list;
+        for(var ent in list){
+          if(info.playerId !== list[ent].playerId &&
+            info.matchCode === list[ent].matchCode){
+              list[ent].matched = socket;
+              info.matched = list[ent].socket;
+              return info.matched;
+          }
+        }
+      }
+    };
+
+    this.clearMatched = function(socket){
+      var sockInfo = this.getSocketInfo(socket);
+      //removing removed socket,
+      //so get his living partner and disconnet first
+      var partner = this.getSocketInfo(sockInfo.matched);
+      if(!partner)//single player
+        return;
+      //disconnect
+      partner.matched = null;
+      //disconnect
+      sockInfo.matchCode = -1;
+      sockInfo.matched = null;
+
+      return (sockInfo.matched === null && partner.matched===null);
+    }
 
     //search the table for a given player id
     this.containsPlayerId = function(pid){
@@ -21,22 +70,24 @@ function playerTable(){
     };
 
     //get the socket OBJECT associated with a player id
-    this.getSocket = function(id){
-
-        var list = this._list;
-        for(var ent in list){
-          if(list[ent].playerId===id){
-            return list[ent].socket;
-          }
-        }
-        return undefined;
+    this.getSocket = function(pid){
+      var list = this._list;
+      for(var ent in list){
+        if(list[ent].playerId === pid)
+          return list[ent].socket;
+      }
+      return undefined;
     };
 
+    this.getSocketInfo = function(socket){
+      var sjson = JSON.stringify(socket);
+      return this._list[sjson];
+    };
     //add an item to the list
     this.addPlayer = function(socket, playerid){
       var sjson = JSON.stringify(socket);
       this._list.push(sjson);
-      this._list[sjson] = this._wrapData(socket, playerid, false);
+      this._list[sjson] = this._wrapData(socket, playerid, null);
       return this._list.length;
     };
 
@@ -44,7 +95,8 @@ function playerTable(){
     this.removePlayer = function(socket){
       var sjson = JSON.stringify(socket);
       if(this._list[sjson]){
-        this._list.splice(sjson,1)
+        this.clearMatched(socket);
+        var removed = this._list.splice(sjson,1);
       }
       return this._list.length;
     };
@@ -53,13 +105,32 @@ function playerTable(){
       return {
         socket: skt,
         playerId: id,
-        matched: mtchd
+        matched: mtchd,
+        matchCode: -1
       };
     };
 
     this.clear = function(){
       this._list = [];
       return this._list.length;
+    }
+
+    this.print = function(){
+      var table = new Table({
+        head: ['Player', 'matchCode', 'matched']
+      , colWidths: [15, 15, 15]
+      });
+    //  console.log(this._list.length);
+      // table is an Array, so you can `push`, `unshift`, `splice` and friends
+      var list = this._list;
+      var count = 1;
+
+      list.forEach(function(ent){
+        table.push(
+            [count++,list[ent].matchCode,list[ent].matched ? list[ent].matched : "null"]
+        );
+      });
+      console.log(table.toString());
     }
 
 }
--- test/test.js
+++ test/test.js
@@ -45,6 +45,9 @@ var pTable = require("../playerTable.js").createTable();
 var socket1 = new Object({name:'socket1'});
 var socket2 = new Object({name:'socket2'});
 var socket3 = new Object({name:'socket3'});
+var socket4 = new Object({name:'socket4'});
+var socket5 = new Object({name:'socket5'});
+var socket6 = new Object({name:'socket6'});
 
 test('playerTable insert and delete', function(t){
 
@@ -67,12 +70,6 @@ test('playerTable insert and check', function(t){
   t.equal(1, pTable.addPlayer(socket1, 1), 'insert 1');
   t.equal(2, pTable.addPlayer(socket2, 2), 'insert 2');
   t.equal(3, pTable.addPlayer(socket3, 3), 'insert 3');
-
-  t.equal(pTable.getPlayers()[JSON.stringify(socket1)].playerId, 1, 'check for correct playerId');
-  t.equal(pTable.getPlayers()[JSON.stringify(socket1)].matched, false, 'check for correct matched state');
-
-  t.equal(pTable.getPlayers()[JSON.stringify(socket2)].playerId, 2, 'check for correct playerId');
-  t.equal(pTable.getPlayers()[JSON.stringify(socket2)].matched, false, 'check for correct matched state');
 
   t.equal(0, pTable.clear(), "clear table test");
 
@@ -98,8 +95,107 @@ test('playerTable getSocket check', function(t){
   t.notEqual(socket3, pTable.getSocket(2), 'check getSocket(2) doesnt return the different socket');
   t.notEqual(socket1, pTable.getSocket(3), 'check getSocket(3) doesnt return the different socket');
 
-  t.equal(0, pTable.clear());
+  t.equal(0, pTable.clear(), "clear table test");
 
   t.end();
 
+});
+
+
+
+test('playerTable containsPlayer check', function(t){
+
+  t.equal(1, pTable.addPlayer(socket1, 1), 'insert player1');
+  t.equal(true, pTable.containsPlayerId(1), 'check for player 1');
+  t.equal(2, pTable.addPlayer(socket2, 2), 'insert player2');
+  t.equal(true, pTable.containsPlayerId(2), 'check for player 2');
+  t.equal(3, pTable.addPlayer(socket3, 3), 'insert player3');
+  t.equal(true, pTable.containsPlayerId(3), 'check for player 3');
+  t.equal(4, pTable.addPlayer(socket4, 4), 'insert player4');
+  t.equal(true, pTable.containsPlayerId(4), 'check for player 4');
+  t.equal(5, pTable.addPlayer(socket5, 5), 'insert player5');
+  t.equal(true, pTable.containsPlayerId(5), 'check for player 5');
+  t.equal(6, pTable.addPlayer(socket6, 6), 'insert player6');
+  t.equal(true, pTable.containsPlayerId(6), 'check for player 6');
+
+  t.equal(true, pTable.containsPlayerId(1), 'check for player 1');
+  t.equal(true, pTable.containsPlayerId(2), 'check for player 2');
+  t.equal(true, pTable.containsPlayerId(3), 'check for player 3');
+  t.equal(true, pTable.containsPlayerId(4), 'check for player 4');
+  t.equal(true, pTable.containsPlayerId(5), 'check for player 5');
+  t.equal(true, pTable.containsPlayerId(6), 'check for player 6');
+
+  t.notEqual(true, pTable.containsPlayerId(40), 'check for player 40 (doesnt exist)');
+  t.notEqual(true, pTable.containsPlayerId(50), 'check for player 50 (doesnt exist)');
+  t.notEqual(true, pTable.containsPlayerId(60), 'check for player 60 (doesnt exist)');
+
+  t.equal(0, pTable.clear(), "clear table test");
+  t.end();
+});
+
+test('playerTable matching check', function(t){
+
+  t.equal(1, pTable.addPlayer(socket1, 1), 'insert player1');
+  t.equal(2, pTable.addPlayer(socket2, 2), 'insert player2');
+  t.equal(3, pTable.addPlayer(socket3, 3), 'insert player3');
+  t.equal(4, pTable.addPlayer(socket4, 4), 'insert player1');
+  t.equal(5, pTable.addPlayer(socket5, 5), 'insert player2');
+  t.equal(6, pTable.addPlayer(socket6, 6), 'insert player3');
+
+  t.equal(true, pTable.addMatchToken(socket1, 10), 'add token to player on socket1');
+  pTable.print();
+  t.equal(true, pTable.addMatchToken(socket2, 20), 'add token to player on socket2');
+  pTable.print();
+  t.equal(true, pTable.addMatchToken(socket3, 30), 'add token to player on socket3');
+  pTable.print();
+  t.equal(true, pTable.addMatchToken(socket4, 40), 'add token to player on socket4');
+  pTable.print();
+  t.equal(true, pTable.addMatchToken(socket5, 50), 'add token to player on socket5');
+  pTable.print();
+  t.equal(true, pTable.addMatchToken(socket6, 60), 'add token to player on socket6');
+  pTable.print();
+
+  t.equal(true, pTable.addMatchToken(socket4, 10), 'matching 4->1');
+  pTable.print();
+  t.equal(true, pTable.addMatchToken(socket5, 20), 'matching 5->2');
+  pTable.print();
+  t.equal(true, pTable.addMatchToken(socket6, 30), 'matching 6->3');
+  pTable.print();
+
+  t.equal(false, pTable.addMatchToken(socket4, 20), 'reset token of player 4 to match player 1');
+  pTable.print();
+  t.equal(false, pTable.addMatchToken(socket5, 30), 'reset token of player 5 to match player 2');
+  pTable.print();
+  t.equal(false, pTable.addMatchToken(socket6, 10), 'reset token of player 6 to match player 3');
+  pTable.print();
+
+  t.equal(true, pTable.clearMatched(socket4), 'disconnecting from partner: 4->1');
+  pTable.print();
+  t.equal(true, pTable.clearMatched(socket5), 'disconnecting from partner: 5->2');
+  pTable.print();
+  t.equal(true, pTable.clearMatched(socket6), 'disconnecting from partner: 6->3');
+  pTable.print();
+
+  t.equal(true, pTable.addMatchToken(socket4, 20), 'matching 4->2');
+  pTable.print();
+  t.equal(true, pTable.addMatchToken(socket5, 30), 'matching 5->3');
+  pTable.print();
+  t.equal(true, pTable.addMatchToken(socket6, 10), 'matching 6->1');
+  pTable.print();
+
+  t.equal(5, pTable.removePlayer(socket1), 'remove player1');
+  pTable.print();
+  t.equal(4, pTable.removePlayer(socket2), 'remove player2');
+  pTable.print();
+  t.equal(3, pTable.removePlayer(socket3), 'remove player3');
+  pTable.print();
+  t.equal(2, pTable.removePlayer(socket4), 'remove player4');
+  pTable.print();
+  t.equal(1, pTable.removePlayer(socket5), 'remove player5');
+  pTable.print();
+  t.equal(0, pTable.removePlayer(socket6), 'remove player6');
+  pTable.print();
+
+  t.equal(0, pTable.clear(), "clear table test");
+  t.end();
 });
