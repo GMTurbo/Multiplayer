14c92bcad7f8b7ea8f7164ab0ad7a5bbe567a6c6
GMTurbo <gmturbo@me.com>
2014-04-10 19:53:52 -0700

    playerTable Upated to stringily keys
    sockets are now JSON.stringified to be used as keys.  the socket is
    saved in the item entry.

--- playerTable.js
+++ playerTable.js
@@ -1,43 +1,69 @@
 
+var randomKeys = require('random-enough');
+
 function playerTable(){
     this._list = [];
     /* entries should look like this
-    *   _list[socket] = {playerId:int, matched: bool};
+    *   _list[JSON.stringify(socket)] = {socket: socket, playerId:int, matched: bool};
     *
     */
 
     //search the table for a given player id
-    this.containsPlayer = function(pid){
+    this.containsPlayerId = function(pid){
       var list = this._list;
       for(var ent in list){
-        if(list[ent].playerId == pid)
+        if(list[ent].playerId === pid)
           return true;
       }
       return false;
     };
 
-    //get the socket associated with a player id
+    this.getPlayers = function(){
+        return this._list;
+    };
+
+    //get the socket OBJECT associated with a player id
     this.getSocket = function(id){
 
         var list = this._list;
         for(var ent in list){
-          if(list[ent].playerid===id){
-            return ent;
+          if(list[ent].playerId===id){
+            return list[ent].socket;
           }
         }
         return undefined;
     };
 
-    this.Add = function(socket, playerid){
-      this._list[socket] = this._wrapData(playerid, false);
+    //add an item to the list
+    this.addPlayer = function(socket, playerid){
+      var sjson = JSON.stringify(socket);
+      this._list.push(sjson);
+      this._list[sjson] = this._wrapData(socket, playerid, false);
+      return this._list.length;
     };
 
-    this._wrapData = function(id, mtchd){
+    //add an item to the list
+    this.removePlayer = function(socket){
+      var sjson = JSON.stringify(socket);
+      if(this._list[sjson]){
+        this._list.splice(sjson,1)
+      }
+      return this._list.length;
+    };
+        // wrap the data up for us
+    this._wrapData = function(skt, id, mtchd){
       return {
+        socket: skt,
         playerId: id,
         matched: mtchd
       };
     };
+
+    this.clear = function(){
+      this._list = [];
+      return this._list.length;
+    }
+
 }
 
 function createTable(){
