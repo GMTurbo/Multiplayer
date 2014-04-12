c17eb39db8e85c2d35542fb65980ed6f31357088
GMTurbo <gmturbo@me.com>
2014-04-12 11:18:10 -0700

    index.js modified
    index.js iteration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var randomKeys = require('random-enough');
 var playerTable = require('./playerTable.js').createTable();
-var redis = require("redis");
+//var redis = require("redis");
 var argv = require('minimist')(process.argv.splice(2));
 
 console.dir("commandline args " + argv);
@@ -14,48 +14,70 @@ var log = argv.save ?
 	require('just-logger').createLogger(argv.save):
 	require('just-logger').createLogger();
 
-var datastore = redis.createClient();
-datastore.on("error", function(err){
-	log.error(err);
-});
+// var datastore = redis.createClient();
+// datastore.on("error", function(err){
+// 	log.error(err);
+// });
 
-var io = require("socket.io").listen(argv.port, '0.0.0.0');
-io.sockets.on('connection', newConnection);
-io.sockets.on('error', function(err){
-	log.error(err);
-});
 
 var newConnection = function(socket){
-	logger.info("new socket connection");
-	socket.emit("success", { response : "200"});
-	socket.on("requestToken", function(data){
+
+	log.info("new socket connection");
+
+	playerTable.addPlayer(socket);
+
+	playerTable.highScoreCallback = function(score){
+		socket.emit('highScore', score);
+	};
+
+	socket.emit('connected', { response : 200 });
+
+	socket.on('requestToken', function(data){
 		var key = randomKeys(12);
 		socket.emit('newToken', {token: key});
 	});
-	socket.on("findPlayer", function(data){
+
+	socket.on('findPlayer', function(data){
 		//data = {
 		//	playerID : int
 		//	}
 
 		var found = playerTable.containsPlayer(data.playerID);
-		socket.emit("playerFound", {playerId: data.playerID, playerFound: found});
+		socket.emit('playerFound', {playerId: data.playerID, playerFound: found});
 
 	});
-	socket.on("registerPlayer", function(data){
+
+	socket.on('registerPlayer', function(data){
 		// data = {
 		// id : created by player
 		//	}
 		if(data.id){
 			playerTable.Add(socket, data.id);
-			socket.emit("registerStatus", {response:"ok"});
+			socket.emit('registerStatus', {response:"ok"});
 		}else{
-			socket.emit("registerStatus", {response:"error"});
+			socket.emit('registerStatus', {response:"error"});
 		}
 	});
-	socket.on("sendScore", function(data){
+
+	socket.on('sendScore', function(data){
 		//data = {
-		//	player: name,
 		//	score: value
 		//	}
+		console.log("score updating");
+		playerTable.setScore(socket, data.score);
+		playerTable.print();
 	});
+
+	socket.on('disconnect', function(){
+		playerTable.removePlayer(socket);
+		log.info("player disconnected");
+	})
 };
+
+var io = require('socket.io').listen(argv.port, '0.0.0.0');
+
+io.sockets.on('connection', newConnection);
+
+io.sockets.on('error', function(err){
+	log.error(err);
+});
